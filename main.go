11e86fc113bb01b1904f8f4e110069b7f6f5a117
gernest <geofreyernest@live.com>
2017-08-01 13:04:27 +0300

    Add (KAlpha).Hex
    This returns hex representation of a klingon alphabet.
    
    We are not covering numbers, only alphabets as the task requires translating names so I assume names don't comprise of numbers.

--- main.go
+++ main.go
@@ -33,6 +33,65 @@ const (
 	KQUOTE               //'
 )
 
+func (k KAlpha) Hex() int {
+	switch k {
+	case KA:
+		return 0xF8D0
+	case KB:
+		return 0xF8D1
+	case KCH:
+		return 0xF8D2
+	case KD:
+		return 0xF8D3
+	case KE:
+		return 0xF8D4
+	case KGH:
+		return 0xF8D5
+	case KH:
+		return 0xF8D6
+	case KI:
+		return 0xF8D7
+	case KJ:
+		return 0xF8D8
+	case KL:
+		return 0xF8D9
+	case KM:
+		return 0xF8DA
+	case KN:
+		return 0xF8DB
+	case KNG:
+		return 0xF8DC
+	case KO:
+		return 0xF8DD
+	case KP:
+		return 0xF8DE
+	case KQ:
+		return 0xF8DF
+	case KQCap:
+		return 0xF8E0
+	case KR:
+		return 0xF8E1
+	case KS:
+		return 0xF8E2
+	case KT:
+		return 0xF8E3
+	case KTLH:
+		return 0xF8E4
+	case KU:
+		return 0xF8E5
+	case KV:
+		return 0xF8E6
+	case KW:
+		return 0xF8E7
+	case KY:
+		return 0xF8E8
+	case KQUOTE:
+		return 0xF8E9
+	default:
+		return 0
+	}
+}
+
 func main() {
 
 }
