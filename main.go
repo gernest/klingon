f68b50980f7f30a0a0263d08e1520254f882e5ae
gernest <geofreyernest@live.com>
2017-08-01 13:00:41 +0300

    Add KAlpha
    This defines tokens that maps to klingon plqaD alphabet

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+// KAlpha token for klingon alphabet
+type KAlpha uint
+
+// klingon alphabets
+const (
+	KA     KAlpha = iota //a
+	KB                   //b
+	KCH                  //ch
+	KD                   //D
+	KE                   //e
+	KGH                  //gh
+	KH                   //h
+	KI                   //I
+	KJ                   //j
+	KL                   //l
+	KM                   //m
+	KN                   //n
+	KNG                  //ng
+	KO                   //o
+	KP                   //p
+	KQ                   //q
+	KQCap                //Q
+	KR                   //r
+	KS                   //s
+	KT                   //t
+	KTLH                 //tlh
+	KU                   //u
+	KV                   //v
+	KW                   //w
+	KY                   //y
+	KQUOTE               //'
+)
+
 func main() {
 
 }
